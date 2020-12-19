9912b1e3c3a2cb2f2bff877d8ff4ee4265e58d1e
Charles Weir <797903+charlesweir@users.noreply.github.com>
2020-12-19 13:11:07 +0000

    Pypi configuration.

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SimpleQualtrics-CharlesWeir", # Replace with your own username
+    version="1.0",
+    author="Charles Weir",
+    author_email="pypi.cafaw@xoxy.net",
+    description="Python module to support Qualtrics APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/charlesweir/SimpleQualtrics",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Topic :: Communications",
+    ],
+    python_requires='>=3.7',
+)
